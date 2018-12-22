17c88e538d6afbaab6bbca8655bc71408a6692d9
Ben <benjamin.dupont@hotmail.com>
2018-12-22 11:56:26 +0100

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  Todoey
+//
+//  Created by Benélo on 22/12/2018.
+//  Copyright © 2018 ben. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title:String = ""
+    var done:Bool = false
+    
+}
